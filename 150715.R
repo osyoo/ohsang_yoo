bbc25d43a30fe504207ba9c6304d3dd32eee3b78
ohsang_yoo <martlet4@gmail.com>
2015-07-15 11:40:10 +0900

    for class 150715

---
+++ 150715.R
@@ -0,0 +1,8 @@
+m.iris<-lm(Sepal.Length~Petal.Length, data=iris)
+iris$pred<-predict(m.iris,iris)
+points(iris$Petal.Length, iris$pred, col="blue",pch=16)
+
+newiris <-data.frame(Petal.Length=seq(1,25,0.5))
+newiris$pred<-predict(m.iris, newiris)
+points(newiris$Petal.Length, newiris$pred,col=3)
+points(newiris$Petal.Length, newiris$pred, col=3, pch=3)
